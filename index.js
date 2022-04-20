fc9c8e33853828cb4bc12957a0c5a791e2946565
Tasnim <tasnim.mostari@gmail.com>
2022-04-20 17:31:31 +0600

    fileUpload added

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express()
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const fileUpload = require('express-fileupload');
 
 const port = process.env.PORT || 5000;
 app.use(cors());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gmioh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
